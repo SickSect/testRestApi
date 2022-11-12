cbc7b16a7f31d53c2ddb4ab72676141ea6e9f96f
sicksect <sleepyboy906sb@gmail.com>
2022-11-13 00:28:03 +0300

    step 0.1

--- src/main/java/com/ugina/serviceApi/controller/MsgController.java
+++ src/main/java/com/ugina/serviceApi/controller/MsgController.java
@@ -5,9 +5,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("message")
 public class MsgController {
+    public List<Map<String, String>> msg = new ArrayList<Map<String, String>>(){
+        {
+            add(new HashMap<String, String>() {{
+                put("id", "1");
+                put("text", "First str");
+            }});
+            add(new HashMap<String, String>() {{
+                put("id", "2");
+                put("text", "Second str");
+            }});
+            add(new HashMap<String, String>() {{
+                put("id", "3");
+                put("text", "Third str");
+            }});
+        }
+    };
     @GetMapping
     public String list(){
         return "Index";
