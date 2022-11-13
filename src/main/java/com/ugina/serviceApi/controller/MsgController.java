3a986c780b4ab576c3eb1fb4b8b98e37a803c29b
sicksect <sleepyboy906sb@gmail.com>
2022-11-13 22:14:15 +0300

    step0.3 add del put post

--- src/main/java/com/ugina/serviceApi/controller/MsgController.java
+++ src/main/java/com/ugina/serviceApi/controller/MsgController.java
@@ -2,10 +2,7 @@ package com.ugina.serviceApi.controller;
 
 import com.ugina.serviceApi.exceptions.NotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,7 +12,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MsgController {
-    public List<Map<String, String>> msg = new ArrayList<Map<String, String>>(){
+    private int counter = 4;
+    private List<Map<String, String>> msg = new ArrayList<Map<String, String>>(){
         {
             add(new HashMap<String, String>() {{
                 put("id", "1");
@@ -37,6 +35,30 @@ public class MsgController {
     }
     @GetMapping("{id}")
     public Map<String, String> getByIndex(@PathVariable String id){
-         return msg.stream().filter(msg->msg.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
+         return getInfo(id);
+    }
+
+    private Map<String, String> getInfo(String id) {
+        return msg.stream().filter(msg -> msg.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> addOne(@RequestBody Map<String, String> info){
+        info.put("id", String.valueOf(counter++));
+        msg.add(info);
+        return info;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> info){
+        Map<String, String> msgFromDb = getInfo(info.get("id"));
+        msgFromDb.putAll(info);
+        msgFromDb.put("id", id);
+        return msgFromDb;
+    }
+    @DeleteMapping("{id}")
+    public void deleteMsg(@PathVariable String id){
+        Map<String, String> msgToDel = getInfo(id);
+        msg.remove(msgToDel);
     }
 }
